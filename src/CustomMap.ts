42193c260b5e10819055e3d700b9ebc69647d024
Angelo <herman256561@gmail.com>
2024-06-28 16:57:30 +0800

    fix customMap

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,43 +1,40 @@
+/// <reference types="@types/google.maps" />
 import { User } from "./User";
 import { Company } from "./Company";
 
 export class CustomMap {
-    
-    private googleMap: google.maps.Map;
-    
-    constructor(divId: string) {
-        
-        this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
-            zoom:1,
-            center: {
-                lat: 0,
-                lng: 0
-            },
-            mapId: 'demo_app',
-        });
-    }
+  private googleMap: google.maps.Map;
 
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
 
-    async addUserMarker(user: User): Promise<void> {
-        const userPosition = { lat: user.location.lat, lng: user.location.lng };
-        const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
-        const marker = new AdvancedMarkerElement({
-            map: this.googleMap,
-            position: userPosition,
-            title: 'user'
-        });
-    };
-    
+  addMarker(mappable: User | Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
 
-    async addCompanyMarker(company: Company): Promise<void> {
-        const comPosition = { lat: company.location.lat, lng: company.location.lng };
-        const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
-        const marker = new AdvancedMarkerElement({
-            map: this.googleMap,
-            position: comPosition,
-            title: 'company'
-        });
-    };
+  // addCompanyMarker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng
+  //     }
+  //   });
+  // }
 }
